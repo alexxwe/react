2867b888811e67c8408f3e6940fdb44803ce78a1
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2024-06-19 00:56:38 +0200

    final update

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -2,35 +2,78 @@ import { useState } from "react";
 import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
 
+const users = [
+  {
+    userName: "AMAZlNGNATURE",
+    name: "nATURE IS AMAZING",
+    isFollowing: true,
+  },
+  {
+    userName: "EvilAFM",
+    name: "Alexelcapo",
+    isFollowing: false,
+  },
+  {
+    userName: "midudev",
+    name: "Miguel Ángel Durán",
+    isFollowing: true,
+  },
+  {
+    userName: "Mouredev",
+    name: "Brais Moure",
+    isFollowing: false,
+  },
+];
+
 export function App() {
-  // const format = (userName) => `@${userName}`;
-
-  //En el useState
-  //1a posicion del array: valor del estado
-  //2a posicion del array: como actualizar el estado
-  // const [name, setName] = useState('MoureDev')
   return (
-    // <> = <React.Fragment>
-    <>
-      <section className="App">
-        <TwitterFollowCard initialIsFollowing={true} userName="AMAZlNGNATURE">
-          nATURE IS AMAZING
-        </TwitterFollowCard>
-
-        <TwitterFollowCard  userName="EvilAFM">
-          Alexlcapo
-        </TwitterFollowCard>
-
-        <TwitterFollowCard initialIsFollowing userName="midudev">
-          Miguel Ángel Durán
-        </TwitterFollowCard>
-
-        {/* <TwitterFollowCard isFollowing userName={name}>
-          Brais Moure
-        </TwitterFollowCard>
-        <button onClick={() => setName("midudev")}>Cambio de @ ejemplo</button> */}
-      </section>
-    </>
-    // </> = </React.Fragment>
+    <section className="App">
+      {users.map(user => {
+        const { userName, name, isFollowing } = user;
+        return (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        );
+      })}
+    </section>
   );
 }
+
+// export function App() {
+//   // const format = (userName) => `@${userName}`;
+
+//   //En el useState
+//   //1a posicion del array: valor del estado
+//   //2a posicion del array: como actualizar el estado
+//   // const [name, setName] = useState('MoureDev')
+
+//   return (
+//     // <> = <React.Fragment>
+//     <>
+//       <section className="App">
+//         <TwitterFollowCard initialIsFollowing={true} userName="AMAZlNGNATURE">
+//           nATURE IS AMAZING
+//         </TwitterFollowCard>
+
+//         <TwitterFollowCard  userName="EvilAFM">
+//           Alexlcapo
+//         </TwitterFollowCard>
+
+//         <TwitterFollowCard initialIsFollowing userName="midudev">
+//           Miguel Ángel Durán
+//         </TwitterFollowCard>
+
+//         {/* <TwitterFollowCard isFollowing userName={name}>
+//           Brais Moure
+//         </TwitterFollowCard>
+//         <button onClick={() => setName("midudev")}>Cambio de @ ejemplo</button> */}
+//       </section>
+//     </>
+//     // </> = </React.Fragment>
+//   );
+// }
