ebeaffa7b3d5705f18acf1e55b538d3dba912556
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2024-07-16 23:14:19 +0200

    MovieFinder show data example
    Lista las películas y muestra el título, año y poster.

--- projects/05-moviefinder/src/App.jsx
+++ projects/05-moviefinder/src/App.jsx
@@ -1,6 +1,10 @@
 import './App.css'
+import responseMovies from './mocks/with-results.json'
+import withoutResults from './mocks/no-results.json'
 
 function App() {
+  const movies = responseMovies.Search 
+  const hasMovies = movies?.length > 0
   return (
     <div className='page'>
       <header>
@@ -11,7 +15,25 @@ function App() {
       </form>
       </header>
       <main>
-        The movies here...
+        {
+          hasMovies
+          ? (
+            <ul>
+              {
+                movies.map(movie => (
+                  <li key={movie.imdbID}>
+                    <h3>{movie.Title}</h3>
+                    <p>{movie.Year}</p>
+                    <img src={movie.Poster} alt={movie.Title}/>
+                  </li>
+                ))
+              }
+            </ul>
+          )
+          :(
+            <p>No movies found for that search</p>
+          )
+        }
       </main>
     </div>
   )
