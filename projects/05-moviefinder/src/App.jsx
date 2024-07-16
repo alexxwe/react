39c2246de65cc414a3e54588af3f00b77f85ffee
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2024-07-16 23:53:05 +0200

    Move to component

--- projects/05-moviefinder/src/App.jsx
+++ projects/05-moviefinder/src/App.jsx
@@ -1,42 +1,38 @@
-import './App.css'
-import responseMovies from './mocks/with-results.json'
-import withoutResults from './mocks/no-results.json'
+import "./App.css";
+// eslint-disable-next-line no-unused-vars
+import responseMovies from "./mocks/with-results.json";
+// eslint-disable-next-line no-unused-vars
+import withoutResults from "./mocks/no-results.json";
+import { Movies } from "./components/Movies";
 
 function App() {
   const movies = responseMovies.Search 
-  const hasMovies = movies?.length > 0
+
+  const mappedMovies = movies?.map(movie => ({
+    id: movie.imdbID,
+    title: movie.Title,
+    year: movie.Year,
+    poster: movie.Poster,
+    type: movie.Type,
+  }))
+
   return (
-    <div className='page'>
+    <div className="page">
+
       <header>
         <h1>MovieFinder</h1>
-      <form className='form'>
-        <input placeholder='Mononoke Hime, Inception, Star Wars... ' />
-        <button type='submit'>Search</button>
-      </form>
+        <form className="form">
+          <input placeholder="Mononoke Hime, Inception, Star Wars... " />
+          <button type="submit">Search</button>
+        </form>
       </header>
+
       <main>
-        {
-          hasMovies
-          ? (
-            <ul>
-              {
-                movies.map(movie => (
-                  <li key={movie.imdbID}>
-                    <h3>{movie.Title}</h3>
-                    <p>{movie.Year}</p>
-                    <img src={movie.Poster} alt={movie.Title}/>
-                  </li>
-                ))
-              }
-            </ul>
-          )
-          :(
-            <p>No movies found for that search</p>
-          )
-        }
+        <Movies movies={mappedMovies} />
       </main>
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
