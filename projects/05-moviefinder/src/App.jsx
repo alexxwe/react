c3d76ae7691bccb0387eac63838309f17857b6fa
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2024-07-19 00:33:48 +0200

    Search controlado y manejo de errores
    Forma controlada significa controlada por React
    Es mejor hacerlo no controlado, es mas sencillo, mas óptimo, suele dar menos problemas y usas javaScript
    
    Controlado tiene sentido dependiendo de lo que quieras hacer

--- projects/05-moviefinder/src/App.jsx
+++ projects/05-moviefinder/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies";
@@ -6,6 +6,7 @@ import { useMovies } from "./hooks/useMovies";
 function App() {
   const {movies} = useMovies()
   const[query, setQuery] = useState('')
+  const[error, setError] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -13,9 +14,28 @@ function App() {
   }
 
   const handleChange = (event) => {
+    const newQuery = event.target.value
+    if(newQuery.startsWith(' ')) return
     setQuery(event.target.value)
   }
 
+  useEffect(() => {
+    if(query === '') {
+      setError('Cant find movie without search term')
+      return
+    }
+
+    if(query.match(/^\d+$/)) {
+      setError('Cant find movie with number')
+      return
+    }
+
+    if(query.length < 3) {
+      setError('The search must be at least 3 characters')
+      return
+    }
+    setError(null)
+  },[query])
 
   return (
     <div className="page">
@@ -23,9 +43,15 @@ function App() {
       <header>
         <h1>MovieFinder</h1>
         <form className="form" onSubmit={handleSubmit}>
-          <input onChange={handleChange} value={query} name="query" placeholder="Inception, Star Wars, Mononoke... " />
+          <input 
+          style={{
+            border: '1px solid transparent',
+            borderColor: error ? 'red' : 'transparent'
+          }}
+          onChange={handleChange} value={query} name="query" placeholder="Inception, Star Wars, Mononoke... " />
           <button type="submit">Search</button>
         </form>
+        {error && <p style={{color: 'red'}}>{error}</p>}
       </header>
 
       <main>
