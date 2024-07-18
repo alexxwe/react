58ac65b2347510aef56292514dda975e490d5158
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2024-07-18 23:54:12 +0200

    possible useRef
    this is an example, not necessary here

--- projects/05-moviefinder/src/App.jsx
+++ projects/05-moviefinder/src/App.jsx
@@ -1,17 +1,28 @@
+import { useRef } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies";
 
 function App() {
-const {movies} = useMovies()
+  const {movies} = useMovies()
+  const inputRef = useRef()
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const inputEl = inputRef.current
+    const value = inputEl.value
+    console.log(value)
+  }
+
 
   return (
     <div className="page">
 
       <header>
         <h1>MovieFinder</h1>
-        <form className="form">
-          <input placeholder="Mononoke Hime, Inception, Star Wars... " />
+        <form className="form" onSubmit={handleSubmit}>
+          <input ref={inputRef} placeholder="Mononoke Hime, Inception, Star Wars... " />
           <button type="submit">Search</button>
         </form>
       </header>
