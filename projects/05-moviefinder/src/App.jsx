ce3d9b83535b19ed78ddae9c3b5068b7a4238009
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2024-08-06 20:11:26 +0200

    Debounce search,
    pnpm install just-debounce-it -E
    
    Demora X tiempo en hacer la búsqueda una vez se deje de actualizar la función, en este caso, una vez se deje de escribir.

--- projects/05-moviefinder/src/App.jsx
+++ projects/05-moviefinder/src/App.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies";
+import debounce from 'just-debounce-it'
 
 function useSearch () {
   const [search, updateSearch] = useState('')
@@ -39,6 +40,13 @@ function App() {
   const{ search, updateSearch, error } = useSearch()
   const {movies, loading, getMovies} = useMovies({ search, sort })
 
+  const debouncedGetMovies = useCallback(
+    debounce(search => {
+      getMovies({ search })
+    }, 500)
+    , [getMovies]
+  )
+
   const handleSubmit = (event) => {
     event.preventDefault()
     getMovies({ search })
@@ -51,7 +59,7 @@ function App() {
   const handleChange = (event) => {
     const newSearch = event.target.value
     updateSearch(newSearch)
-    getMovies({ search: newSearch })
+    debouncedGetMovies(newSearch)
   }
 
   return (
