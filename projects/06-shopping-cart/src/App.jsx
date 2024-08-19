8b558675c5b5376ea361ad6af18b8919dcff2204
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2024-08-19 23:30:55 +0200

    Footer
    Footer to show my filters info in 'development' or 'production'

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -2,6 +2,8 @@ import { products as initialProducts} from "./mocks/products.json"
 import { Products } from "./components/Products"
 import { useState } from "react"
 import { Header } from "./components/Header"
+import { Footer } from "./components/Footer"
+import { IS_DEVELOPMENT } from "./config"
 
 function useFilters () {
   const [filters, setFilters] = useState({
@@ -17,18 +19,19 @@ function useFilters () {
       )
     })
   }
-  return { filterProducts, setFilters}
+  return { filters, filterProducts, setFilters}
 }
 
 function App() {
   const [products] = useState(initialProducts)
-  const { filterProducts, setFilters} = useFilters()
+  const { filters, filterProducts, setFilters} = useFilters()
   const filteredProducts = filterProducts(products)
 
   return (
 <>
 <Header changeFilters={setFilters} />
 <Products products={filteredProducts} />
+{IS_DEVELOPMENT && <Footer filters={ filters } />}
 </>
 
   )
