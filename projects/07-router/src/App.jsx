e041b9b66724ff78a824ffa1895af056f50a90a8
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2024-09-12 00:14:17 +0200

    SPA creation
    This time using buttons to see if it works properly

--- projects/07-router/src/App.jsx
+++ projects/07-router/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+
+const NAVIGATION_EVENT = 'pushState'
+
+function navigate (href) {
+  window.history.pushState({}, '', href)
+  const navigationEvent = new Event(NAVIGATION_EVENT)
+  window.dispatchEvent(navigationEvent)
+}
 
 function HomePage() {
   return (
     <>
     <h1> Home </h1>
-    <p> Exaple page doing react router</p>
-    <a href='/about'> About us </a>
+    <p> Example page making a react router</p>
+    <button onClick={() => navigate('/about')}> About us </button>
     </>
   )
 }
@@ -19,13 +27,25 @@ function AboutPage() {
     <img src='https://avatars.githubusercontent.com/u/91157929?s=400&u=09341df1b71e23749a0a9f4f255f52d684fe1fa8&v=4' alt='Profile photo' style={{ borderRadius: '50%' }} ></img>
     <p> Hi! Im Alex, software Developer, this is my React Router practice.</p>
     </div>
-    <a href='/'> Home </a>
+    <button onClick={() => navigate('/')}> Home </button>
     </>
   )
 }
 
 function App() {
   const[currentPath, setCurrentPath ] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+
+    return() => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange)
+    }
+  }, [])
   return (
     <main> 
       {currentPath === '/' && <HomePage />}
