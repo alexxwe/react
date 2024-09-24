f6d90b2847140ae10dbda448c36481a89fdb0ad4
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2024-09-24 23:44:54 +0200

    make it more declarative

--- projects/07-router/src/App.jsx
+++ projects/07-router/src/App.jsx
@@ -4,27 +4,44 @@ import { EVENTS } from './assets/consts'
 import HomePage from './pages/Home'
 import AboutPage from './pages/About'
 
+const routes = [
+  {
+    path: '/',
+    Component: HomePage
+  },
+  {
+    path: '/about',
+    Component: AboutPage
+  }
+]
 
-function App() {
+function Router ({ routes = [], defaultComponent: DefaultComponent = () => <h1>404</h1> }) {
+
   const[currentPath, setCurrentPath ] = useState(window.location.pathname)
-
+  
   useEffect(() => {
     const onLocationChange = () => {
       setCurrentPath(window.location.pathname)
     }
-
+  
     window.addEventListener(EVENTS.PUSHSTATE, onLocationChange)
     window.addEventListener(EVENTS.POPSTATE, onLocationChange)
-
+  
     return() => {
       window.removeEventListener(EVENTS.PUSHSTATE, onLocationChange)
       window.addEventListener(EVENTS.POPSTATE, onLocationChange)
     }
   }, [])
+
+  const Page = routes.find(({ path }) => path === currentPath)?.Component
+  return Page ? <Page /> : <DefaultComponent />
+}
+
+
+function App() {
   return (
     <main> 
-      {currentPath === '/' && <HomePage />}
-      {currentPath === '/about' && <AboutPage />}
+      <Router routes={routes} />
     </main>
   )
 }
