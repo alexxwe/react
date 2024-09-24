deabdb1513ec27894c71920383832687579d8311
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2024-09-24 23:46:25 +0200

    create component Router
    Create component Router to make it more declarative

--- projects/07-router/src/App.jsx
+++ projects/07-router/src/App.jsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-import { EVENTS } from './assets/consts'
 import HomePage from './pages/Home'
 import AboutPage from './pages/About'
+import { Router } from './Router'
 
-const routes = [
+const approutes = [
   {
     path: '/',
     Component: HomePage
@@ -15,33 +14,10 @@ const routes = [
   }
 ]
 
-function Router ({ routes = [], defaultComponent: DefaultComponent = () => <h1>404</h1> }) {
-
-  const[currentPath, setCurrentPath ] = useState(window.location.pathname)
-  
-  useEffect(() => {
-    const onLocationChange = () => {
-      setCurrentPath(window.location.pathname)
-    }
-  
-    window.addEventListener(EVENTS.PUSHSTATE, onLocationChange)
-    window.addEventListener(EVENTS.POPSTATE, onLocationChange)
-  
-    return() => {
-      window.removeEventListener(EVENTS.PUSHSTATE, onLocationChange)
-      window.addEventListener(EVENTS.POPSTATE, onLocationChange)
-    }
-  }, [])
-
-  const Page = routes.find(({ path }) => path === currentPath)?.Component
-  return Page ? <Page /> : <DefaultComponent />
-}
-
-
 function App() {
   return (
     <main> 
-      <Router routes={routes} />
+      <Router routes={approutes} />
     </main>
   )
 }
