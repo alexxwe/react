675246a77739d461120f261f4d1a30a5d8945c80
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2024-10-08 21:52:48 +0200

    lazy load
    La pagina recorre todo el código y al leer el codigo de arriba a abajo y detectar los imports los renderiza también aunque no se usen. De esta manera solo se ejecutaran cuando se necesita.

--- projects/07-router/src/App.jsx
+++ projects/07-router/src/App.jsx
@@ -1,25 +1,32 @@
-import './App.css'
-import HomePage from './pages/Home'
-import AboutPage from './pages/About'
-import { Router } from './Router'
-import { Route } from './Route'
-import Page404 from './pages/404'
-import SearchPage from './pages/Search'
+import "./App.css"
+// import HomePage from "./pages/Home"
+// import AboutPage from './pages/About'
+import Page404 from "./pages/404"
+import SearchPage from "./pages/Search"
+import { lazy, Suspense } from "react"
+
+import { Router } from "./Router"
+import { Route } from "./Route"
+
+const LazyHomePage = lazy(() => import("./pages/Home"))
+const LazyAboutPage = lazy(() => import("./pages/About"))
 
 const approutes = [
   {
-    path: '/search/:query',
-    Component: SearchPage
-  }
+    path: "/search/:query",
+    Component: SearchPage,
+  },
 ]
 
 function App() {
   return (
-    <main> 
-      <Router routes={approutes} defaultComponent={Page404}>
-        <Route path='/' Component={HomePage} />
-        <Route path='/about' Component={AboutPage} />
-      </Router>
+    <main>
+      <Suspense fallback={<div>Loading ...</div>} >
+        <Router routes={approutes} defaultComponent={Page404}>
+          <Route path="/" Component={LazyHomePage} />
+          <Route path="/about" Component={LazyAboutPage} />
+        </Router>
+      </Suspense>
     </main>
   )
 }
